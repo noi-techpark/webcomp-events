dd4b79e5798754b95b33d9b58a4670dc146ed876
maxglio <massimo.maglio.06@gmail.com>
2023-06-15 13:37:52 +0200

    removed .bz.it and added .com

--- code/api/config.js
+++ code/api/config.js
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-export const BASE_PATH_TOURISM = "https://tourism.opendatahub.bz.it/v1";
+export const BASE_PATH_TOURISM = "https://tourism.opendatahub.com/v1";
 export const BASE_PATH_TOURISM_EVENT = `${BASE_PATH_TOURISM}/Event`;
 export const BASE_PATH_TOURISM_EVENT_REDUCED = `${BASE_PATH_TOURISM}/EventReduced`;
 export const BASE_PATH_TOURISM_EVENTTOPICS = `${BASE_PATH_TOURISM}/EventTopics`;
